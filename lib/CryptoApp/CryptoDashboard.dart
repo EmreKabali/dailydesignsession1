19ddc90e74b95305643a959fd5d02b841ca04f27
Emre Kabali <kabali68@gmail.com>
2022-05-20 22:10:44 +0300

    crypto part 3

--- lib/CryptoApp/CryptoDashboard.dart
+++ lib/CryptoApp/CryptoDashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CryptoDashboard extends StatefulWidget {
@@ -11,8 +12,10 @@ class _CryptoDashboardState extends State<CryptoDashboard> {
   @override
   Widget build(BuildContext context) {
     bool isSearchActive = false;
+    Size size = MediaQuery.of(context).size;
     TextEditingController contr = TextEditingController();
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 218, 241, 240),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: SafeArea(
@@ -61,6 +64,73 @@ class _CryptoDashboardState extends State<CryptoDashboard> {
                           ])
                 ],
               ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: size.height * 0.25,
+                width: size.width,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5), blurRadius: 2),
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.2), blurRadius: 2),
+                    ],
+                    borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.bitcoin,
+                              color: Colors.orange,
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              'Total Balance',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Container(
+                          child: Text(
+                            '0.1287654',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            '4,990.99 USD',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green),
+                          ),
+                        ),
+                      ],
+                    )),
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://img.favpng.com/4/11/4/pie-chart-diagram-computer-icons-png-favpng-XWFGY96iazHaCicv7UJM3dyLK.jpg'))),
+                    ))
+                  ],
+                ),
+              )
             ],
           ),
         ),
